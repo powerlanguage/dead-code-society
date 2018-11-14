2b8c851788c0a0bc656923ad0a8dd4e1596b2ea2
Josh Wardle <josh.wardle@gmail.com>
2018-11-13 23:08:33 -0800

    Prettier

--- deadCodeSociety.js
+++ deadCodeSociety.js
@@ -5,8 +5,8 @@ Make sure you've updated maniftest.json with your phabricator URL
 
 */
 
-const increased = "color: #28a745;"
-const decreased = "color: #cb2431;"
+const increased = 'color: #28a745;';
+const decreased = 'color: #cb2431;';
 
 const sectionHeader = `
 <div class="phui-property-list-section-header">
@@ -15,7 +15,7 @@ const sectionHeader = `
         Dead Code Society 🔪
     </span>
 </div>
-`
+`;
 
 const sectionBody = `
 <div class="phui-property-list-text-content">
@@ -29,28 +29,38 @@ const sectionBody = `
         </p>
     </div>
 </div>
-`
+`;
 
 const e = document.getElementsByClassName('phui-property-list-section')[0];
-e.insertAdjacentHTML( 'beforeend', sectionHeader );
-e.insertAdjacentHTML( 'beforeend', sectionBody );
+e.insertAdjacentHTML('beforeend', sectionHeader);
+e.insertAdjacentHTML('beforeend', sectionBody);
 
 // Used to extract line modified counts from the ToC
-const LINES_REGEX = /\((\d*)/
+const LINES_REGEX = /\((\d*)/;
 
 // Status can be Added, Deleted, Modified
-const statuses = Array.from(document.querySelectorAll('td.differential-toc-char')).map(item => item.textContent);
+const statuses = Array.from(
+  document.querySelectorAll('td.differential-toc-char')
+).map(item => item.textContent);
 // Lines changes as reported by phabricator (used for file additions and deletions)
-const linesChanged = Array.from(document.querySelectorAll('td.differential-toc-file')).map((item) => {
+const linesChanged = Array.from(
+  document.querySelectorAll('td.differential-toc-file')
+).map(item => {
   const match = item.textContent.match(LINES_REGEX);
   return match ? parseInt(match[1]) : 0;
-})
+});
 // convert #change-xz9IumBgJjfM to xz9IumBgJjfM
-const ids = Array.from(document.querySelectorAll('td.differential-toc-file'))
-  .map(item => item.querySelector('a') .getAttribute("href") .substring(8));
+const ids = Array.from(
+  document.querySelectorAll('td.differential-toc-file')
+).map(item =>
+  item
+    .querySelector('a')
+    .getAttribute('href')
+    .substring(8)
+);
 
 // Track counted diff ids so we don't recount if view is toggled
-const countedDiffIds = []
+const countedDiffIds = [];
 
 const updateStats = (id, additions, deletions) => {
   if (countedDiffIds.includes(id)) {
@@ -63,27 +73,32 @@ const updateStats = (id, additions, deletions) => {
   const newDelta = oldDelta + additions - deletions;
   const newAdditions = oldAdditions + additions;
   const newDeletions = oldDeletions + deletions;
-  
+
   document.getElementById('delta').textContent = newDelta;
   document.getElementById('added').textContent = newAdditions;
   document.getElementById('deleted').textContent = newDeletions;
-  document.getElementById('deltaContainer').style = newDelta >= 0 ? increased : decreased;
+  document.getElementById('deltaContainer').style =
+    newDelta >= 0 ? increased : decreased;
 };
 
 // Once diff loads
 const handleMutation = (mutationsList, id) => {
-  for(let mutation of mutationsList) {
-    if(mutation.addedNodes.length) {
-      for(let node of mutation.addedNodes) {
+  for (let mutation of mutationsList) {
+    if (mutation.addedNodes.length) {
+      for (let node of mutation.addedNodes) {
         if (node.className.includes('differential-diff')) {
-          additions = Array.from(node.querySelectorAll('td.new'))
-            .filter(line => line.className === 'new' || line.className === 'new new-full' || line.className === 'right new').length;
+          additions = Array.from(node.querySelectorAll('td.new')).filter(
+            line =>
+              line.className === 'new' ||
+              line.className === 'new new-full' ||
+              line.className === 'right new'
+          ).length;
           deletions = node.querySelectorAll('td.old').length;
           updateStats(id, additions, deletions);
         }
-      };
+      }
     }
-  };
+  }
 };
 
 // If the change was a file addition or deletion we can just use the counts we parsed from phabricator text.
@@ -95,8 +110,10 @@ ids.forEach((id, index) => {
     updateStats(id, 0, linesChanged[index]);
   } else {
     const change = document.querySelector(`#diff-change-${id}`);
-    const config = { childList: true, subtree: true }
-    const observer = new MutationObserver((mutationsList, observer) => { handleMutation(mutationsList, id) });
+    const config = { childList: true, subtree: true };
+    const observer = new MutationObserver((mutationsList, observer) => {
+      handleMutation(mutationsList, id);
+    });
     observer.observe(change, config);
   }
 });
